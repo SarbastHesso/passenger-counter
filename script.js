ffc497abe116a3ccaf4b11827297f8062d607faf
Sarbast Hesso <sarbast.hesso@gmail.com>
2024-01-11 23:45:22 +0100

    finished build the count app

---
+++ script.js
@@ -0,0 +1,24 @@
+const countEl = document.querySelector('#count-el');
+const saveEl = document.querySelector("#save-el");
+
+let count = 0;
+let countArray = [];
+
+const increment = () => {
+    count ++;
+    countEl.innerText = count;
+}
+
+const save = () => {
+    countArray.push(count);
+    saveEl.innerText = `Previous entries: ${countArray.join(" - ")}`;
+    count = 0;
+    countEl.innerText = count;
+}
+
+// const save = () => {
+//     let countStr = count + ' - '
+//     saveEl.textContent += countStr;
+//     count = 0;
+//     countEl.innerText = count;
+// }
